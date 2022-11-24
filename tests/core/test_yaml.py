78a2db8fa52b74602d0271cf39008b70a8357921
Nikhil Woodruff <nikhil@policyengine.org>
2022-11-24 00:04:42 +0000

    Changelog

--- tests/core/test_yaml.py
+++ tests/core/test_yaml.py
@@ -133,21 +133,6 @@ def test_failing_shell_script():
             subprocess.check_call(command, stdout=devnull, stderr=devnull)
 
 
-def test_shell_script_with_reform():
-    yaml_path = os.path.join(yaml_tests_dir, "test_with_reform_2.yaml")
-    command = [
-        "policyengine-core",
-        "test",
-        yaml_path,
-        "-c",
-        "policyengine_core.country_template",
-        "-r",
-        "policyengine_core.country_template.reforms.removal_basic_income.removal_basic_income",
-    ]
-    with open(os.devnull, "wb") as devnull:
-        subprocess.check_call(command, stdout=devnull, stderr=devnull)
-
-
 def test_shell_script_with_extension():
     tests_dir = os.path.join(openfisca_extension_template.__path__[0], "tests")
     command = [

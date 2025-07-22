9b324d2f2259f6e439f4b03a0fbcc4b740fbf4e7
Max Ghenis <mghenis@gmail.com>
2025-07-22 15:38:37 -0400

    Remove accidentally added log files

--- claude_code_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define the log file
-LOGFILE="claude_code_output.log"
-
-# Run Claude Code and redirect both stdout and stderr to the log file
-# while also displaying them in the terminal
-claude "$@" > >(tee -a "$LOGFILE") 2> >(tee -a "$LOGFILE" >&2)
